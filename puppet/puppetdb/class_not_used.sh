7067114dc6d60a7e43e5c1c3eb1aed655da51050
Jan Vansteenkiste <jan.vansteenkiste@ugent.be>
2022-10-24 16:10:30 +0200

    Get a list of nodes NOT using a class

---
+++ puppet/puppetdb/class_not_used.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "USAGE: $0 <classname>"
+  echo "# Lists all nodes that use a class"
+  exit 64;
+fi;
+
+normalize_classname() {
+  IFS="::"
+  local class=( $1 )
+  echo "${class[@]^}" | sed 's@\s\+@::@g'
+}
+
+class="$( normalize_classname "$1" )"
+
+diff -u0 -uN <(curl -Gs http://localhost:8080/pdb/query/v4/resources/Class/$class | jq '.[].certname' | sort) \
+     <(curl -Gs http://localhost:8080/pdb/query/v4/nodes | jq '.[].certname' | sort)
